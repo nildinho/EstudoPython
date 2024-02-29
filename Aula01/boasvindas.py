a749f6de0e28c38d82d2ec7ca5adefbac4157935
Vanildo Lima <nildinho.anahi@hotmail.com>
2024-02-29 15:29:13 -0300

    Init: Iniciando o estudo em Python. Exibindo mensagens

---
+++ Aula01/boasvindas.py
@@ -0,0 +1,7 @@
+# Vamos criar uma variável para armazenar 0
+# nome do usuário
+nome = "admin"
+print("Olá, "+nome+". Seja bem vindo!")
+print("Escreva seu novo nome de usuário:")
+nome = input("Digite aqui:")
+print("O seu novo usuário é :"+nome)
